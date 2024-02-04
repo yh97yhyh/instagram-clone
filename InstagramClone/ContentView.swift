787661016524fd9205c48a84120d97035cba0049
영현 <yh97yhyh@naver.com>
2024-02-05 07:46:25 +0900

    first commit

---
+++ InstagramClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstagramClone
+//
+//  Created by 영현 on 2/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by 영현 on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
