b09aef70b9fcde0b2ba6f481aa5201c78ee3b52e
영현 <yh97yhyh@naver.com>
2024-02-05 09:45:54 +0900

    feed view ui

---
+++ InstagramClone/Core/Feed/FeedCell.swift
@@ -0,0 +1,100 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by 영현 on 2/5/24.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack {
+            // image + username
+            HStack {
+                Image("naruto-profile")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                Text("naruto")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Spacer() // 왼쪽으로 붙임
+            }
+            .padding(.leading, 8)
+            
+            // post image
+            Image("naruto-1")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .clipShape(Rectangle())
+            
+            // action button
+            HStack {
+                Button {
+                    print("Like post")
+                } label: {
+                    Image(systemName: "heart")
+                        .imageScale(.large)
+                }
+                
+                Button {
+                    print("Share post")
+
+                } label: {
+                    Image(systemName: "bubble.right")
+                        .imageScale(.large)
+
+                }
+                
+                Button {
+                    print("Like post")
+                } label: {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+
+                }
+                
+                Spacer()
+            }
+            .padding(.leading, 8)
+            .padding(.top, 4)
+            .foregroundColor(.black)
+            
+            
+            // likes label
+            Text("23 likes")
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+            
+            // caption label
+            HStack {
+                // + 로 텍스트를 하나로 합침
+                Text("naruto ").fontWeight(.semibold) +
+                Text("This is some test caption for now This is some test caption for now")
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.footnote)
+            .padding(.leading, 10)
+            .padding(.top, 1)
+            
+            Text("6h ago")
+                .font(.footnote)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.top, 1)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ InstagramClone/Core/Feed/FeedView.swift
@@ -0,0 +1,43 @@
+//
+//  FeedView.swift
+//  InstagramClone
+//
+//  Created by 영현 on 2/5/24.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+                .padding(.top, 8)
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram_logo_black")
+                        .resizable()
+                        .frame(width: 100, height: 32)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    FeedView()
+}
+
