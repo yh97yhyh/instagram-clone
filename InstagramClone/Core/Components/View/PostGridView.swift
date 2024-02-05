da832f8b6daef3ff91aceb0f788f0b8b50328959
영현 <yh97yhyh@naver.com>
2024-02-06 06:24:05 +0900

    fetch user posts done

--- InstagramClone/Core/Components/View/PostGridView.swift
+++ InstagramClone/Core/Components/View/PostGridView.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct PostGridView: View {
-    var posts: [Post]
+    @StateObject var viewModel: PostGridViewModel
+    
+    init(user: User) {
+        self._viewModel = StateObject(wrappedValue: PostGridViewModel(user: user))
+    }
     
     private let gridItems: [GridItem] = [
         .init(.flexible(), spacing: 1),
@@ -17,11 +22,11 @@ struct PostGridView: View {
     ]
     
     private let imageDimension: CGFloat = (UIScreen.main.bounds.width / 3) - 1
-    
+     
     var body: some View {
         LazyVGrid(columns: gridItems, spacing: 1) {
-            ForEach(posts) { post in
-                Image(post.imageUrl)
+            ForEach(viewModel.posts) { post in
+                KFImage(URL(string: post.imageUrl))
                     .resizable()
                     .scaledToFill()
                     .frame(width: imageDimension, height: imageDimension)
@@ -32,5 +37,5 @@ struct PostGridView: View {
 }
 
 #Preview {
-    PostGridView(posts: Post.MOCK_POSTS)
+    PostGridView(user: User.MOCK_USERS[0])
 }
